2cae5377133d4a2e02b3e5369af8a6cb46385e74
Gorbacheva Sasha <90442800+GorbachevaSasha@users.noreply.github.com>
2021-09-28 23:03:54 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,119 +1,208 @@
 """
 Lab 1
-Language detection
+A concordance extraction
 """
 
 
-def tokenize(text: str) -> list or None:
+def tokenize(text: str) -> list:
     """
-    Splits a text into tokens, converts the tokens into lowercase,
-    removes punctuation and other symbols from words
-    :param text: a text
-    :return: a list of lower-cased tokens without punctuation
+    Splits sentences into tokens, converts the tokens into lowercase, removes punctuation
+    :param text: the initial text
+    :return: a list of lowercased tokens without punctuation
+    e.g. text = 'The weather is sunny, the man is happy.'
+    --> ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
     """
-    return 
+    if not isinstance(text, str):
+        return []
+
+    clean_tokens = []
+    for token in text.lower().split():
+        word = ''
+        for character in token:
+            if character.isalpha():
+                word += character
+        if word:
+            clean_tokens.append(word)
+    return clean_tokens
 
 
-def remove_stop_words(tokens: list, stop_words: list) -> list or None:
+def remove_stop_words(tokens: list, stop_words: list) -> list:
     """
     Removes stop words
     :param tokens: a list of tokens
     :param stop_words: a list of stop words
     :return: a list of tokens without stop words
+    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy']
+    stop_words = ['the', 'is']
+    --> ['weather', 'sunny', 'man', 'happy']
     """
-    pass
+    if not isinstance(tokens, list) or not isinstance(stop_words, list):
+        return []
+
+    stop_words_set = frozenset(stop_words)
+    return [item for item in tokens if item not in stop_words_set]
 
 
-def calculate_frequencies(tokens: list) -> dict or None:
+def calculate_frequencies(tokens: list) -> dict:
     """
     Calculates frequencies of given tokens
-    :param tokens: a list of tokens
+    :param tokens: a list of tokens without stop words
     :return: a dictionary with frequencies
+    e.g. tokens = ['weather', 'sunny', 'man', 'happy']
+    --> {'weather': 1, 'sunny': 1, 'man': 1, 'happy': 1}
     """
-    pass
+    if not isinstance(tokens, list) or None in tokens:
+        return {}
+    frequencies_dict = dict()
+    for word in tokens:
+        if word not in frequencies_dict.keys():
+            frequencies_dict[word] = tokens.count(word)
+    return frequencies_dict
 
 
-def get_top_n_words(freq_dict: dict, top_n: int) -> list or None:
+def get_top_n_words(freq_dict: dict, top_n: int) -> list:
     """
     Returns the most common words
     :param freq_dict: a dictionary with frequencies
-    :param top_n: a number of the most common words
+    :param top_n: a number of the most common words to return
     :return: a list of the most common words
+    e.g. tokens = ['weather', 'sunny', 'man', 'happy', 'and', 'dog', 'happy']
+    top_n = 1
+    --> ['happy']
     """
-    pass
+    if not isinstance(freq_dict, dict) or not isinstance(top_n, int) or None in freq_dict:
+        return []
+    frequencies_and_words_sorted = sorted(list(freq_dict.items()), key=lambda i: i[1], reverse=True)
+    result = []
+    for word_freq in frequencies_and_words_sorted[:top_n]:
+        result.append(word_freq[0])
+    return result
 
 
-def create_language_profile(language: str, text: str, stop_words: list) -> dict or None:
+def get_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int) -> list:
     """
-    Creates a language profile
-    :param language: a language
-    :param text: a text
-    :param stop_words: a list of stop words
-    :return: a dictionary with three keys – name, freq, n_words
+    Gets a concordance of a word
+    A concordance is a listing of each occurrence of a word in a text,
+    presented with the words surrounding it
+    :param tokens: a list of tokens
+    :param word: a word-base for a concordance
+    :param left_context_size: the number of words in the left context
+    :param right_context_size: the number of words in the right context
+    :return: a concordance
+    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
+                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
+    word = 'happy'
+    left_context_size = 2
+    right_context_size = 3
+    --> [['man', 'is', 'happy', 'the', 'dog', 'is'], ['dog', 'is', 'happy', 'but', 'the', 'cat']]
     """
-    pass
+    if not isinstance(tokens, list) or not isinstance(word, str) \
+            or None in tokens:
+        return []
+    if left_context_size is True or right_context_size is True \
+            or not isinstance(left_context_size, int) or not isinstance(right_context_size, int):
+        return []
+    if left_context_size < 0:
+        left_context_size = 0
+    if right_context_size < 0:
+        right_context_size = 0
+    if left_context_size == 0 and right_context_size == 0:
+        return []
+    contexts = []
+    for index, element in enumerate(tokens):
+        if element == word:
+            contexts.append(tokens[index - left_context_size:index + right_context_size + 1])
+    return contexts
 
 
-def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int) -> float or None:
+def get_adjacent_words(tokens: list, word: str, left_n: int, right_n: int) -> list:
     """
-    Compares profiles and calculates the distance using top n words
-    :param unknown_profile: a dictionary
-    :param profile_to_compare: a dictionary
-    :param top_n: a number of the most common words
-    :return: the distance
+    Gets adjacent words from the left and right context
+    :param tokens: a list of tokens
+    :param word: a word-base for the search
+    :param left_n: the distance between a word and an adjacent one in the left context
+    :param right_n: the distance between a word and an adjacent one in the right context
+    :return: a list of adjacent words
+    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
+                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
+    word = 'happy'
+    left_n = 2
+    right_n = 3
+    --> [['man', 'is'], ['dog, 'cat']]
     """
-    pass
+    if not isinstance(tokens, list) or not isinstance(word, str) or not isinstance(left_n, int) or \
+            not isinstance(right_n, int) or isinstance(left_n, bool) or isinstance(right_n, bool) or \
+            right_n < 0 or left_n < 0 or word not in tokens:
+        return []
+
+    word_index = list()
+    request = list()
+
+    for i in range(len(tokens)):
+        if word in tokens[i]:
+            word_index.append(i)
+    for index in word_index:
+        if left_n > len(tokens[:index]):
+            request.append([tokens[index - 1]])
+        elif right_n > len(tokens[index:]):
+            request.append([tokens[index + right_n]])
+        else:
+            need = [tokens[index - left_n], tokens[index + right_n]]
+            if word in need:
+                need.remove(word)
+            request.append(need)
+    return request
 
 
-def detect_language(unknown_profile: dict, profile_1: dict, profile_2: dict, top_n: int) -> str or None:
+def read_from_file(path_to_file: str) -> str:
     """
-    Detects the language of an unknown profile
-    :param unknown_profile: a dictionary
-    :param profile_1: a dictionary
-    :param profile_2: a dictionary
-    :param top_n: a number of the most common words
-    :return: a language
+    Opens the file and reads its content
+    :return: the initial text in string format
     """
-    pass
+    with open(path_to_file, 'r', encoding='utf-8') as fs:
+        data = fs.read()
+
+    return data
 
 
-def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict, top_n: int) -> list or None:
+def write_to_file(path_to_file: str, content: list):
     """
-    Compares profiles and calculates some advanced parameters
-    :param unknown_profile: a dictionary
-    :param profile_to_compare: a dictionary
-    :param top_n: a number of the most common words
-    :return: a dictionary with 7 keys – name, score, common, sorted_common, max_length_word,
-    min_length_word, average_token_length
+    Writes the result in a file
     """
-    pass
+    with open(path_to_file, 'w', encoding='utf-8') as fs:
+        for text in content:
+            fs.write(" ".join(text).join("\n"))
 
 
-def detect_language_advanced(unknown_profile: dict, profiles: list, languages: list, top_n: int) -> str or None:
+def sort_concordance(tokens: list, word: str, left_context_size: int, right_context_size: int, left_sort: bool) -> list:
     """
-    Detects the language of an unknown profile within the list of possible languages
-    :param unknown_profile: a dictionary
-    :param profiles: a list of dictionaries
-    :param languages: a list of possible languages
-    :param top_n: a number of the most common words
-    :return: a language
+    Gets a concordance of a word and sorts it by either left or right context
+    :param tokens: a list of tokens
+    :param word: a word-base for a concordance
+    :param left_context_size: the number of words in the left context
+    :param right_context_size: the number of words in the right context
+    :param left_sort: if True, sort by the left context, False – by the right context
+    :return: a concordance
+    e.g. tokens = ['the', 'weather', 'is', 'sunny', 'the', 'man', 'is', 'happy',
+                    'the', 'dog', 'is', 'happy', 'but', 'the', 'cat', 'is', 'sad']
+    word = 'happy'
+    left_context_size = 2
+    right_context_size = 3
+    left_sort = True
+    --> [['dog', 'is', 'happy', 'but', 'the', 'cat'], ['man', 'is', 'happy', 'the', 'dog', 'is']]
     """
-    pass
+    contexts = get_concordance(tokens, word, left_context_size, right_context_size)
+    if not isinstance(left_sort, bool):
+        return []
+    if not contexts:
+        return []
+    if (left_sort and left_context_size < 0) or (not left_sort and right_context_size < 0):
+        return []
 
+    if left_sort:
+        return sorted(contexts)
 
-def load_profile(path_to_file: str) -> dict or None:
-    """
-    Loads a language profile
-    :param path_to_file: a path
-    :return: a dictionary with three keys – name, freq, n_words
-    """
-    pass
-
-
-def save_profile(profile: dict) -> int:
-    """
-    Saves a language profile
-    :param profile: a dictionary
-    :return: 0 if everything is ok, 1 if not
-    """
-    pass
+    word_index = contexts[0].index(word)
+    right_contexts = [(context[word_index + 1:], context[:word_index + 1]) for context in contexts]
+    right_contexts_sorted = sorted(right_contexts)
+    return [sorted_right_context[1] + sorted_right_context[0] for sorted_right_context in right_contexts_sorted]
